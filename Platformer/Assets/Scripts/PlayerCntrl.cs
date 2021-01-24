e83379bbf11caaad6f681d4d6c261eb02c887673
alx1115 <klyaznikalexa2003@gmail.com>
2021-01-24 23:37:39 +0200

    first commit

---
+++ Platformer/Assets/Scripts/PlayerCntrl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCntrl : MonoBehaviour{
+
+    public float speed = 20f;
+    private Rigidbody2D rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update(){
+        float moveX = Input.GetAxis ("Horizontal");
+        rb.MovePosition (rb.position + Vector2.right * moveX * Time.deltaTime * speed);
+        }
+}
---
+++ Platformer/Assets/Scripts/PlayerSprites.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerSprites : MonoBehaviour
+{
+
+    public Sprite R;
+    public Sprite RUp;
+    private SpriteRenderer spriteRenderer;
+
+    void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null) 
+            spriteRenderer.sprite = R;
+    }
+
+    // Input.GetKeyDown (KeyCode.Space)
+    void Update(){
+        deltaSprite();
+    }
+    void deltaSprite(){
+        if (Input.GetAxis ("Horizontal")>0){
+            spriteRenderer.sprite = R;
+            spriteRenderer.flipX = false;
+        }else if(Input.GetAxis ("Horizontal")<0){
+            spriteRenderer.sprite = R;
+            spriteRenderer.flipX = true;
+        }else if(Input.GetAxis ("Horizontal")==0){
+            spriteRenderer.sprite = RUp;
+        }
+    }
+}
+
+
+
+       
+   
+     
+
